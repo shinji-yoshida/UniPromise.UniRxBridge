48d7e0856ccf0350059477c4439048061c8912e2
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-12-02 17:27:50 +0900

    AddToPromiseExtensions

---
+++ Assets/Scripts/UniPromise/UniRxBridge/AddToPromiseExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UniRx;
+using System.Collections.Generic;
+using System;
+
+namespace UniPromise.UniRxBridge {
+	public static class AddToPromiseExtensions {
+		public static Promise<T> AddTo<T>(this Promise<T> promise, ICollection<IDisposable> container) {
+			if(promise.IsPending)
+				UniRx.DisposableExtensions.AddTo(promise, container);
+
+			return promise;
+		}
+
+		public static Promise<T> AddTo<T>(this Promise<T> promise, GameObject gameObject) {
+			if(promise.IsPending)
+				UniRx.DisposableExtensions.AddTo(promise, gameObject);
+
+			return promise;
+		}
+		
+		public static Promise<T> AddTo<T>(this Promise<T> promise, Component component) {
+			if(promise.IsPending)
+				UniRx.DisposableExtensions.AddTo(promise, component);
+
+			return promise;
+		}
+	}
+}
