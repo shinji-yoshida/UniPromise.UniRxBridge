6070b15b22e53cda75fa11dc8b64353a3eb4fef1
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-12-02 17:50:00 +0900

    PromiseOnNextObservableExtensions

---
+++ Assets/Scripts/UniPromise/UniRxBridge/PromiseOnNextObservableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+namespace UniPromise.UniRxBridge {
+	public static class PromiseOnNextObservableExtensions {
+		public static Promise<T> PromiseOnNext<T>(this IObservable<T> source) {
+			return new SubscribedPromise<T>(source);
+		}
+	}
+}
---
+++ Assets/Scripts/UniPromise/UniRxBridge/SubscribedPromise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UniRx;
+
+namespace UniPromise.UniRxBridge {
+	public class SubscribedPromise<T> : Deferred<T> {
+		IDisposable subscription;
+
+		public SubscribedPromise(IObservable<T> source) {
+			subscription = source.Take(1).Subscribe(t => this.Resolve(t), e => this.Reject(e), () => this.Dispose());
+		}
+
+		public override void Dispose () {
+			base.Dispose();
+			subscription.Dispose();
+		}
+	}
+}
