139ca99a65c612f24b0d92fd61cbaa7deb5f7877
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-12-02 17:36:05 +0900

    UnitPromise

---
+++ Assets/Scripts/UniPromise/UniRxBridge/UnitPromise.cs
@@ -0,0 +1,14 @@
+﻿using UniRx;
+using System;
+
+namespace UniPromise.UniRxBridge {
+	public static class UnitPromise {
+		public static Promise<Unit> Resolved = Promises.Resolved<Unit>(Unit.Default);
+
+		public static Promise<Unit> Rejected(Exception e) {
+			return Promises.Rejected<Unit>(e);
+		}
+
+		public static Promise<Unit> Disposed = Promises.Disposed<Unit>();
+	}
+}
