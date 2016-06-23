f4d5de89d5f6a39fedb75e7fbde11223125dab95
Shinji.Yoshida <yoshida.shinji.gm@gmail.com>
2016-06-23 17:06:18 +0900

    AsObservable

---
+++ Assets/Scripts/UniPromise/UniRxBridge/AsObservablePromiseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UniRx;
+
+
+namespace UniPromise.UniRxBridge {
+	public static class AsObservablePromiseExtensions {
+		public static IObservable<T> AsObservable<T>(this Promise<T> promise) {
+			return Observable.Create<T>(observer => promise.Clone ().Done (t => {
+				observer.OnNext (t);
+				observer.OnCompleted ();
+			}).Fail (e => observer.OnError (e)));
+		}
+	}
+}
